d8348fd8f4859b2c84fc1cee5c4402ce46e79ff3
ahmed-alfarouq <mediocre23534@gmail.com>
2025-02-27 10:27:50 +0200

    update main banner component

---
+++ src/components/MainBanner.tsx
@@ -0,0 +1,40 @@
+import styles from "@/styles/features/MainBanner.module.css";
+import Link from "next/link";
+
+type MainBannerProps = {
+  title: string;
+  description: string;
+  bgUrl: string;
+  links?: {
+    title: string;
+    icon?: React.ReactNode;
+    url: string;
+    type: string;
+  }[];
+};
+
+const MainBanner = ({ title, description, bgUrl, links }: MainBannerProps) => {
+  const customStyles = {
+    background: `url(${bgUrl}) center center /cover`,
+  };
+  return (
+    <section className={styles.main_banner} style={customStyles}>
+      <div className={styles.overlay}></div>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.description}>{description}</p>
+      <div className={styles.btns}>
+        {links?.map((link) => (
+          <Link
+            href={link.url}
+            className={`${styles.btn} ${styles[link.type]}`}
+          >
+            {link?.icon}
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default MainBanner;
